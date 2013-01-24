1acc2eba21f3c2a6f17447f21b434c06da3e50d4
greenmobile <tranelis@gmino.de>
2013-01-24 18:30:31 +0100

    Signed-off-by: greenmobile <tranelis@gmino.de>

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $latitude = "52.26196";
 
 $titel = "Problematische Radwege in Braunschweig";
 $description = "Wer kennt Stellen in Braunschweig, die für Radfahrer gefährlich/nervig sind? Die Piraten sammeln, bitte hier eintragen.";
-$header_text = "Die Braunschweiger Piraten sammeln hier problematische Radwege in ihrer Stadt";
+$header_text = "Hier sammeln wir problematische Radwege in ihrer Stadt";
 
 
 $db_adress = "localhost";
